cd2c44ba514829b863e514495415c426ca26fe4f
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-08-25 11:29:49 +0200

    Bezig - 01:11:32 - DataBinding - @ViewChild

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
   title: string = "List of Post";
   messagePost: string = "Message Post";
+
+  postParentMessage: string = "Message from postParent"
+
+  @Input() childProp: string = "";
+
+
 
   constructor() { }
 
