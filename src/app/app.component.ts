c388e370c4924e6983ac3d33b7575efd07c532f8
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-08-26 17:14:04 +0200

    Afgewerkt - 03:06:08 - Directives - ngIf Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,11 @@ export class AppComponent {
 
   // objArray: Array<any> = [
   objArray: Array<{ id: number, postTitle: string }> = [
-    { id: 1, postTitle: "post1" },
-    { id: 2, postTitle: "post2" },
-    { id: 3, postTitle: "post3" },
-    { id: 4, postTitle: "post4" },
-    { id: 5, postTitle: "post5" },
+    // { id: 1, postTitle: "post1" },
+    // { id: 2, postTitle: "post2" },
+    // { id: 3, postTitle: "post3" },
+    // { id: 4, postTitle: "post4" },
+    // { id: 5, postTitle: "post5" },
   ];
 
   addNew(): void {
