8f8f5c9f44bb577732c6bb4fac66f1edd8b193c1
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-08-26 20:38:27 +0200

    Afgewerkt - 02:21:19 - Two-Way DataBinding - Component Toegevoegd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { PostComponent } from './post/post.component';
-import { trigger } from '@angular/animations';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,46 +6,4 @@ import { trigger } from '@angular/animations';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // # String Interpolation
-  message: string = "Message from Typescript Component File";
-
-  // # Property Binding
-  imgUrl: string = "https://chocolates.com.pe/wp-content/uploads/2019/02/ole-ole.png";
-
-  // # Class Binding
-  trueFalse: boolean = false;
-
-  // # Event Binding
-  buttonClick(): void {
-    console.log("Ole Ole");
-  }
-
-  // # Event Filtering
-  onKeyUp(eventObject: any) {
-    if (eventObject.keyCode === 13) {
-      console.log("Enter key pressed");
-    }
-  }
-
-  onKeyUp2() {
-    console.log("Enter key pressed");
-  }
-
-  // # Template Variable
-  // js-manier
-  onKeyUp3(event: any) {
-    // Waarde van input-tag verkrijgen
-    console.log(event.target.value);
-  }
-
-  // Angular manier
-  onKeyUp4(inputWaarde: HTMLInputElement) {
-    // Waarde van input-tag verkrijgen
-    console.log(inputWaarde.value);
-  }
-
-  // # Two-way DataBinding
-  userName!: string;
-
-
 }
